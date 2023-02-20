f8ed1c30a19885084055393d08e78dd35c600b46
Forte Zhuo <hedikasmanto@gmail.com>
2023-02-20 08:31:04 +0700

    Minor fix

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -4,13 +4,15 @@ import crypto from "crypto"
 export function encrypt(this: PatternOption, link: string) {
     const url = new URL(link)
     const urlChunks = url.pathname.split("/")
-    const hmac = crypto.createHmac("sha256", this.secret)
 
     urlChunks[urlChunks.length - 1] = `${urlChunks[urlChunks.length - 1]}${url.search}`
 
     this.filter.forEach((chunk) => {
         const idx = this.chunks.indexOf(chunk)
-        urlChunks[idx] = hmac.update(urlChunks[idx]).digest("hex")
+        if (urlChunks[idx]) {
+            const hmac = crypto.createHmac("sha256", this.secret)
+            urlChunks[idx] = hmac.update(urlChunks[idx]).digest("hex")
+        }
     })
 
     return url.origin + urlChunks.join("/")
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,23 @@
 import { urlcrypt } from "../src/index"
 
-const option = { secret: "Secret", pattern: "http://example.com/api/auth/:id" }
+const option = { secret: "Secret", pattern: "http://example.com/api/oauth2/:id" }
 const link = "https://sub.domain.com/api/oauth2/success?redirect=https://other.domain.com/api/auth"
+const link2 = "https://sub.domain.com/api/"
 const encrypted = "https://sub.domain.com/api/oauth2/c6bff2779a356bb52b01f0efabc0a818b1527b9821866be71c947aa6c97c59e3"
+
+const crypto = urlcrypt(option)
 
 describe('urlcrypt', () => {
     it('Register Option', () => {
-        expect(urlcrypt(option).option).toEqual(option);
+        expect(crypto.option).toEqual(option);
     });
     it('Encrypt', () => {
-        expect(urlcrypt(option).encrypt(link)).toEqual(encrypted);
+        expect(crypto.encrypt(link)).toEqual(encrypted);
+    });
+    it('Encrypt 2', () => {
+        expect(crypto.encrypt(link2)).toEqual(link2);
     });
     it('Verify', () => {
-        expect(urlcrypt(option).verify(encrypted, link)).toEqual(true);
+        expect(crypto.verify(encrypted, link)).toEqual(true);
     });
 });
